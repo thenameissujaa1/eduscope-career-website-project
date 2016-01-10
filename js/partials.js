2d3886563aa9aac5fccefe1ce3ace1bd11152a9b
Balraj <polltery@gmail.com>
2016-01-10 15:32:21 +0400

    Updated Login page to match the theme

--- js/partials.js
+++ js/partials.js
@@ -2,8 +2,15 @@
 var currentTitle = $(document).find("title").text();
 
 // Check if the partial is appropriate to load for this page, we do this by checking the title
-if(currentTitle === "Eduscope Homepage"){
-    $('#header').load("partials/navbar.html");  
-    $('#contact-us').load("partials/contactus.html");
-    $('#footer').load("partials/footer.html");
-}
+$('#header').load("partials/navbar.html");  
+$('#contact-us').load("partials/contactus.html");
+$('#footer').load("partials/footer.html");
+
+$(window).load(function(){
+    if(currentTitle === "Eduscope Homepage"){
+        $('#nav-home').addClass('active');
+    }
+    if(currentTitle === "Eduscope Login"){
+        $('#nav-login').addClass('active');
+    }
+});
