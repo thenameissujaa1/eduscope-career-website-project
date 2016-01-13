ee1161ce18aaf4652795f82ce79f2ebdd887f5b3
Balraj <polltery@gmail.com>
2016-01-13 14:30:47 +0400

    Added About Us Section and optional sections are included in partials.js

--- js/partials.js
+++ js/partials.js
@@ -5,6 +5,23 @@ var currentTitle = $(document).find("title").text();
 $('#header').load("partials/navbar.html");  
 $('#contact-us').load("partials/contactus.html");
 $('#footer').load("partials/footer.html");
+$('#about').load("partials/aboutus.html");
+$('#animated-number').load("partials/funfacts.html");
+$('#testimonial').load("partials/testimonial.html");
+/*
+    To add fun facts use the code below
+    <!---->
+    <section id="animated-number">
+    </section>
+    <!--./-->
+    
+    To add testimonial add the code below
+    <!---->
+    <section id="testimonial">
+    </section>
+    <!--./-->
+    
+ */
 
 $(window).load(function(){
     if(currentTitle === "Eduscope Homepage"){
