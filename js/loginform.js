b8ad20929fd348fdf477dea089a05d638bbb63e7
Balraj <polltery@gmail.com>
2016-01-21 02:24:38 +0400

    Added full signup function with backend validation

--- js/loginform.js
+++ js/loginform.js
@@ -23,6 +23,10 @@ var login_validator = new FormValidator('login_form', [{
         $('#login_validation_errors').show(250);
         $('#login_validation_errors').html(errorString);
         
+    }else{
+    
+        // If the login is a success
+    
     }
 });
 
@@ -42,6 +46,10 @@ var signup_validator = new FormValidator('signup_form', [{
     name: 'signup_password_confirm',
     display: 'password confirmation',
     rules: 'required|matches[signup_password]'
+},{
+    name: 'signup_terms_checkbox',
+    display: 'Terms and condition',
+    rules: 'required'
 }], function(errors, event) {
     if (errors.length > 0) {
         // Show the errors
@@ -54,6 +62,48 @@ var signup_validator = new FormValidator('signup_form', [{
         $('#signup_validation_errors').show(250);
         $('#signup_validation_errors').html(errorString);
         
+    }else{
+        
+        // Prevents changing of header with user's information
+        event.preventDefault();
+        
+        // If the signup is a success
+        // serialize array into JSON format
+        var postData = $('#signup_form').serializeArray();
+        
+        // Show a message     
+        $('#signup_validation_errors').show(250);
+        $('#signup_validation_errors').html('Processing...');
+        /*
+        Doing the AJAX way..
+        $.ajax({
+            type: 'POST',
+            url: 'partials/functions/validate_signup.php',
+            data: postData,
+            success: function(data){
+                console.log(data);  
+            }
+        })
+        */
+        // use jQuery post shorthand to post data
+        $.post('partials/functions/validate_signup.php', postData, function(data){
+            
+            // If ajax success
+            console.log(data);
+            if(data == "success"){
+                window.location.href = 'validate.html';
+                console.log('data is success');
+            }else{
+                $('#signup_validation_errors').html(data);
+            }
+
+        }).fail(function(data){
+            
+            // If ajax fails
+            $('#signup_validation_errors').html('Error while contacting the server, Please try again');
+        
+        }); // end post
+
     }
 });
 
@@ -70,7 +120,7 @@ function formToggle() {
         
         // Hide login elements
         $("#login_form").slideUp(250);
-        $('#login_validation_errors').slideUp(250);
+        $("#login_validation_errors").slideUp(250);
         
         // Modify text based elements for signup
         $("#formHeading").fadeOut(250, function () {
@@ -104,6 +154,5 @@ function formToggle() {
         $('#login_form').slideDown(250);
     }
 }
-
 
 
