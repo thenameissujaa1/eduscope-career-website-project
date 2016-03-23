0248b9feb50904aeeeccc8fff89fd114e8960c7e
Balraj <polltery@gmail.com>
2016-03-23 19:39:41 +0400

    Added backend API for getting a relation between 2 users (for mentor)
    Usage: getInfo.php?type=relation&receiver=id
    type : relation | This will tell the script to look for what relation the 2 user's have
    receiver : <id> | The user_id of the reciever
    Response is sent as..
    RelationStatus : (0|1|2|3) |
        2 means user is not a mentor
        1 means user is already mentor,
        3 means request pending,
        0 means user is sending request to self
        relation : <string> | The message to be sent depending on the relation, 3 cases user sends to themselves, or the other user is already a mentor, or the other
        user's request is pending, or the sending user is not linked with the receiving user.

--- partials/functions/getInfo.php
+++ partials/functions/getInfo.php
@@ -5,23 +5,41 @@ session_start();
 
 /*
     getInfo is a common service script for all requests that replies with a JSON query based on the arguments provided
-
+    
+    BASIC STATUS RESPONSE (Doesn't apply to some parts)
     Initlaize response array to be sent as JSON, each response will have ['status'] attribute to notify the frontend about any errors
     The response['status'] of type int attribute will mean the following
     0 : user not logged in
     1 : user logged in, but no profile details found, first time login
     2 : user logged in, profile details found
     
+    TYPE: list
     Alternatively, you can also get a list of things without concering the user id
     getInfo.php?type=list&from=tableName
     type : list | This will tell the script that you want a list of things
     from : <table name> | This will be the table you would like to get 
     
+    TYPE: profile
     You can also get info of another user by setting the type as profile and the user as the username of the profile
     for example..
     getInfo.php?type=profile&user=username
     type : profile | This will tell the script to get profile details of a specified user
     user : <username> | The user name to look at
+    
+    TYPE: relation
+    You can check the relation between the user and the mentor (The user recieving the mentor request)
+    for example..
+    getInfo.php?type=relation&receiver=id
+    type : relation | This will tell the script to look for what relation the 2 user's have
+    receiver : <id> | The user_id of the reciever
+    Response is sent as..
+    RelationStatus : (0|1|2|3) | 
+    2 means user is not a mentor
+    1 means user is already mentor, 
+    3 means request pending, 
+    0 means user is sending request to self
+    relation : <string> | The message to be sent depending on the relation, 3 cases user sends to themselves, or the other user is already a mentor, or the other
+    user's request is pending, or the sending user is not linked with the receiving user.
     
 */
 
@@ -30,6 +48,7 @@ $response = array();
 if(isset($_SESSION['loggedin_user']) == false || checkType($_GET['type']) == false){
     
     $response['status'] = 0;
+    $response['error'] = 'User not logged in';
     send_response($response);
 
 }else{
@@ -101,6 +120,48 @@ if(isset($_SESSION['loggedin_user']) == false || checkType($_GET['type']) == fal
                 send_response($response);
             }
             break;
+        case 'relation';
+            $receiver_id = $_GET['receiver'];
+            // Check if the receiver is numeric
+            if(preg_match("/[^0-9]/i", $receiver_id, $match)){
+                $response['status'] = 0;
+                send_response($response);
+            }else{
+                // First we get the user_id
+                $sql = 'SELECT user_id FROM user WHERE user_id = "'.$receiver_id.'"';
+                $result = $mysqli->query($sql);
+                $receiver_id = $result->fetch_assoc(); // fetches as an array
+                if($result->num_rows > 0){
+                    if($receiver_id['user_id'] == $user_id){
+                        $response['relationStatus'] = 0;
+                        $response['relation'] = 'Can\'t send request to self';
+                        send_response($response);
+                    }else{
+                        $sql = 'SELECT * FROM request_pending WHERE fk_sender_user_id = '.$user_id.' AND fk_acceptor_user_id = '.$receiver_id['user_id'];
+                        $result = $mysqli->query($sql);
+                        if($result->num_rows == 0){
+                            $sql = 'SELECT * FROM userMentor WHERE fk_user_id = '.$user_id.' AND fk_mentor_id = '.$receiver_id['user_id'];
+                            $result = $mysqli->query($sql);
+                            if($result->num_rows > 0){
+                                $response['relationStatus'] = 1;
+                                $reponse['relation'] = 'This User is already your mentor';
+                                send_response($reponse);
+                            }else{
+                                $response['relationStatus'] = 2;
+                                $response['relation'] = 'This User is not your mentor';
+                                send_response($response);
+                            }
+                        }else{
+                            $response['relationStatus'] = 3;
+                            $response['relation'] = 'Mentor request pending';
+                            send_response($response);
+                        }
+                    }
+                }else{
+                    $response['status'] = 0;
+                    send_response($response);
+                }
+            }
         default: 
             $sql = 'SELECT * FROM '.$type.' WHERE '.$type.'_fk_user_id = '.$user_id;
     }
@@ -116,7 +177,7 @@ function send_response($data){
 }
 
 function checkType($type){
-    if(preg_match("/^(userDetail|user|list|profile)$/", $type, $match)){
+    if(preg_match("/^(userDetail|user|list|profile|relation)$/", $type, $match)){
         return true;
     }else{
         return false;
