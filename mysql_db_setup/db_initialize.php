8ce50efdf86f16a1e57412edf11313be62adef65
Balraj <polltery@gmail.com>
2016-03-26 20:13:20 +0400

    Updated db files - Removed backup sql because it was useless - Updated the db.sql to support mentors The following username and passwords can be used for testing: u: username p: password u: username2 p: password u: username3 p: password You can also create your own account and test it.

--- mysql_db_setup/db_initialize.php
+++ mysql_db_setup/db_initialize.php
@@ -1,12 +1,14 @@
 <?php
 
-/*  Purpose of this script is to check if the DB exisits or not
+/*  Purpose of this script is to check if the DB exists or not
  *  if not then the DB will be created on your localhost
  *  Along with the DB all the tables will also be imported from .sql file
+ *  However, if the db does exists, and if you are trying to update, you will have to DROP the old database, and then run this script
+ *  NOTE: Usage if this script is not important, you can simply import the sql file in your database.
  */
 
 $hostname = 'localhost';
-$port = '3306';
+$port = '3306';                 // MAKE SURE PORT IS CORRECT
 $username = 'root';
 $password = '';
 $db = 'eduscope';
