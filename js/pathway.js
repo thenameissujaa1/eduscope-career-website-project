37b996d067d5e19a17cc1f15bf0a976b5c34bb03
Balraj <polltery@gmail.com>
2016-04-03 10:18:42 +0400

    Added pathway option for bachelors with honours.

--- js/pathway.js
+++ js/pathway.js
@@ -33,20 +33,20 @@ function initializePathway(){
                                             's4');
                                     break;
                                     case 'AS Level/S5':
-                                        html = renderOption_3('You could do...',
-                                            'Foundation Year','Bachelors','A levels/S6',
-                                            'primary','warning','primary',
-                                            'Some choose to do a foundation year instead of A levels','Not many universities support this','A levels/S6 is recommended by most universities',
-                                            'foundation_year','bachelors','s6',
+                                        html = renderOption_4('You could do...',
+                                            'Foundation Year','Bachelors','A levels/S6','Bachelors with honours',
+                                            'primary','warning','primary','warning',
+                                            'Some choose to do a foundation year instead of A levels','Not many universities support this','A levels/S6 is recommended by most universities','Get honours degree instead of an ordinary degree',
+                                            'foundation_year','bachelors','s6','bhons',
                                             's5');
                                     break;
                                     case 'A Level/S6':
-                                        html = renderOption_1('You should do...',
-                                            'Bachelors',
-                                            'primary',
-                                            'You will start your Bachelors',
-                                            'bachelors',
-                                            'foundation_year');
+                                        html = renderOption_2('You could do...',
+                                            'Bachelors','Bachelors with honours',
+                                            'warning','primary',
+                                            'You will start your Bachelors','Recommended path',
+                                            'bachelors','bhons',
+                                            's6');
                                     break;
                                     case 'Bachelors':
                                         html = renderOption_2('You could do...',
@@ -55,6 +55,14 @@ function initializePathway(){
                                             'Work experience is always good','Masters will help you improve in your field of study',
                                             'job','masters',
                                             'bachelors')
+                                    break;
+                                    case 'Bachelors with Honours':
+                                        html = renderOption_3('You could do...',
+                                            'Job','PhD','Masters',
+                                            'primary','primary','primary',
+                                            'Work experience is always good','If you love your field of study','Masters will help you improve in your field of study',
+                                            'job','phd','masters',
+                                            'bhons')
                                     break;
                                     case 'Masters':
                                         html = renderOption_2('You could do...',
@@ -140,13 +148,13 @@ function renderOption_3(title,a,b,c,type_a,type_b,type_c,help_a,help_b,help_c,da
     var html = '<div class="col-xs-12 text-center"><h1 class="glyphicon glyphicon-arrow-down"></h1></div>';
     html += '<div class="col-xs-12 panel panel-default">';
     html += '<div class="panel-heading">'+title+'</div>';
-    html += '<div class="panel-body"><div class="row"><div class="col-xs-4">';
+    html += '<div class="panel-body"><div class="row"><div class="col-sm-4">';
     html += '<button class="btn btn-lg btn-'+type_a+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_a+'">'+a+'</button>';
     html += '<p>'+help_a+'</p>';
-    html += '</div><div class="col-xs-4">';
+    html += '</div><div class="col-sm-4">';
     html += '<button class="btn btn-lg btn-'+type_b+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_b+'">'+b+'</button>';
     html += '<p>'+help_b+'</p>';
-    html += '</div><div class="col-xs-4">';
+    html += '</div><div class="col-sm-4">';
     html += '<button class="btn btn-lg btn-'+type_c+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_c+'">'+c+'</button>';
     html += '<p>'+help_c+'</p>';
     html += '</div></div>';
@@ -155,44 +163,108 @@ function renderOption_3(title,a,b,c,type_a,type_b,type_c,help_a,help_b,help_c,da
     return html;
 }
 
+function renderOption_4(title,a,b,c,d,type_a,type_b,type_c,type_d,help_a,help_b,help_c,help_d,data_to_a,data_to_b,data_to_c,data_to_d,data_from){
+    var html = '<div class="col-xs-12 text-center"><h1 class="glyphicon glyphicon-arrow-down"></h1></div>';
+    html += '<div class="col-xs-12 panel panel-default">';
+    html += '<div class="panel-heading">'+title+'</div>';
+    html += '<div class="panel-body"><div class="row"><div class="col-sm-3">';
+    html += '<button class="btn btn-sm btn-'+type_a+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_a+'">'+a+'</button>';
+    html += '<p>'+help_a+'</p>';
+    html += '</div><div class="col-sm-3">';
+    html += '<button class="btn btn-sm btn-'+type_b+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_b+'">'+b+'</button>';
+    html += '<p>'+help_b+'</p>';
+    html += '</div><div class="col-sm-3">';
+    html += '<button class="btn btn-sm btn-'+type_c+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_c+'">'+c+'</button>';
+    html += '<p>'+help_c+'</p>';
+    html += '</div><div class="col-sm-3">';
+    html += '<button class="btn btn-sm btn-'+type_d+'" id="future_select" data-from="'+data_from+'" data-to="'+data_to_d+'">'+d+'</button>';
+    html += '<p>'+help_d+'</p>';
+    html += '</div></div>';    
+    html += '<div class="panel-footer"><button class="btn btn-xs btn-primary" id="save_pathway" data-final="'+data_from+'">Stop and save pathway</button></div>';
+    html += '</div></div>';
+    return html;
+}
+
+$(document).on('click','#save_pathway',function(){
+    $(this).removeClass('btn-primary').addClass('btn-success').parent().attr('style','background-color: #90EE90').parent().find('button').attr('disabled','disabled');
+})
+
+var future_pathway = [];
+
 $(document).on('click','#future_select',function(){
     var from = $(this).data('from');
     var to = $(this).data('to');
-    $(this).attr('class','btn btn-lg btn-success');
+    future_pathway.push({"from":from,"to":to});
+    if($(this).hasClass('btn-sm'))
+        $(this).attr('class','btn btn-sm btn-success');
+    else
+        $(this).attr('class','btn btn-lg btn-success');
     $(this).parent().parent().find('button').attr('disabled','disabled');
     $(this).parent().parent().parent().find('.panel-footer').hide();
     var html = '';
     if(from == 's4' && to == 's5'){
-        html = renderOption_3('You could do...',
-            'Foundation Year','Bachelors','A levels/S6',
-            'primary','warning','primary',
-            'Some choose to do a foundation year instead of A levels','Not many universities support this','A levels/S6 is recommended by most universities',
-            'foundation_year','bachelors','s6',
+        html = renderOption_4('You could do...',
+            'Foundation Year','Bachelors','A levels/S6','Bachelors with honours',
+            'primary','warning','primary','warning',
+            'Some choose to do a foundation year instead of A levels','Not many universities support this','A levels/S6 is recommended by most universities','Get honours degree instead of an ordinary degree',
+            'foundation_year','bachelors','s6','bhons',
             's5');
     }
     if(from == 's4' && to == 'foundation_year'){
-        html = renderOption_1('You should do...',
-            'Bachelors',
-            'primary',
-            'You will start your Bachelors',
-            'bachelors',
+        html = renderOption_2('You could do...',
+            'Bachelors','Bachelors with honours',
+            'warning','primary',
+            'You will start your Bachelors','Recommended path',
+            'bachelors','bhons',
             'foundation_year');
     }
     if(from == 's5' && to == 'foundation_year'){
-        html = renderOption_1('You should do...',
-            'Bachelors',
-            'primary',
-            'You will start your Bachelors',
-            'bachelors',
+        html = renderOption_2('You could do...',
+            'Bachelors','Bachelors with honours',
+            'warning','primary',
+            'You will start your Bachelors','Recommended path',
+            'bachelors','bhons',
             'foundation_year');
     }
     if(from == 's5' && to == 's6'){
-        html = renderOption_1('You should do...',
-            'Bachelors',
-            'primary',
-            'You will start your Bachelors',
-            'bachelors',
-            'foundation_year');
+        html = renderOption_2('You could do...',
+            'Bachelors','Bachelors with honours',
+            'warning','primary',
+            'You will start your Bachelors','Recommended path',
+            'bachelors','bhons',
+            's6');
+    }
+    if((from == 's6' && to == 'bhons') || (from == 's5' && to == 'bhons') || (from == 'foundation_year' && to == 'bhons')){
+        html = renderOption_3('You could do...',
+            'Job','PhD','Masters',
+            'primary','primary','primary',
+            'Work experience is always good','If you love your field of study','Masters will help you improve in your field of study',
+            'job','phd','masters',
+            'bhons')
+    }
+    if(from == 'bhons' && to == 'job'){
+        html = renderOption_2('You could do...',
+            'Masters','PhD',
+            'primary','primary',
+            'Masters will help you improve in your field of study, and help you get a promotion','If you love your field of study, then go for it!',
+            'masters','phd',
+            'job')
+    }
+    if(from == 'bhons' && to == 'phd'){
+        html = renderOption_2('You could do...',
+            'Job','PhD',
+            'primary','primary',
+            'Work experience is always good','Another one!',
+            'job','phd',
+            'phd')
+    }
+    if(from == 'bhons' && to == 'masters'){
+        html = renderOption_2('You could do...',
+            'Job','Masters',
+            'primary','primary',
+            'Work experience is always good','Masters will help you improve in your field of study',
+            'job','masters',
+            'bachelors')
     }
     if((from == 's6' && to == 'bachelors') || (from == 'foundation_year' && to == 'bachelors') || (from == 's5' && to == 'bachelors')){
         html = renderOption_2('You could do...',
